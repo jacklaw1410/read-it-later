af85a0d444d7d8a6bc7d28f1e65b74c71dffd1fc
jacklaw1410 <jacklaw1410@gmail.com>
2020-05-30 21:25:25 +0800

    refactor: extract bookmark component

---
+++ src/components/bookmark.js
@@ -0,0 +1,38 @@
+import Box from '@material-ui/core/Box';
+import Chip from '@material-ui/core/Chip';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import { formatDistanceToNow } from 'date-fns';
+import React from 'react';
+
+const Bookmark = ({ bookmark }) => {
+  return (
+    <>
+      <Box>
+        <Link href={bookmark.url} target="_blank" rel="noopener noreferrer">
+          {bookmark.title}
+        </Link>
+      </Box>
+      <Box>
+        <Box display="flex" alignItems="center">
+          <Typography variant="caption">
+            <i>{`${formatDistanceToNow(new Date(bookmark.added))} ago`}</i>
+          </Typography>
+
+          {bookmark.tags.map(tag => (
+            <Box mx={0.5} key={tag.name}>
+              <Chip
+                variant="outlined"
+                size="small"
+                label={<Typography variant="caption">{tag.name}</Typography>}
+                style={{ height: 'auto' }}
+              />
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Bookmark;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,8 @@
 import Box from '@material-ui/core/Box';
-import Chip from '@material-ui/core/Chip';
-import Hidden from '@material-ui/core/Hidden';
-import Link from '@material-ui/core/Link';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
-import { formatDistanceToNow } from 'date-fns';
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
+import Bookmark from '../components/bookmark';
 import Header from '../components/header';
 import SEO from '../components/seo';
 
@@ -61,37 +53,7 @@ const IndexPage = () => {
         <Box display="grid" style={{ gridGap: 8 }}>
           {bookmarks.map(bookmark => (
             <Box key={bookmark.url} pb={1} borderBottom="solid 1px #dcdcdc">
-              <Box>
-                <Link
-                  href={bookmark.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {bookmark.title}
-                </Link>
-              </Box>
-              <Box>
-                <Box display="flex" alignItems="center">
-                  <Typography variant="caption">
-                    <i>{`${formatDistanceToNow(
-                      new Date(bookmark.added)
-                    )} ago`}</i>
-                  </Typography>
-
-                  {bookmark.tags.map(tag => (
-                    <Box mx={0.5} key={tag.name}>
-                      <Chip
-                        variant="outlined"
-                        size="small"
-                        label={
-                          <Typography variant="caption">{tag.name}</Typography>
-                        }
-                        style={{ height: 'auto' }}
-                      />
-                    </Box>
-                  ))}
-                </Box>
-              </Box>
+              <Bookmark bookmark={bookmark} />
             </Box>
           ))}
         </Box>
